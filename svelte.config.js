6fa8c3a47e0d467ac5ab9937522b1b7f0f58038a
Hanif ADN <hanifsumarno@gmail.com>
2026-05-30 12:47:30 +0700

    Disable relative paths to fix root navigation issue on GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,8 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || '/docs-fitforecast'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || '/docs-fitforecast',
+			relative: false
 		},
 		prerender: {
 			entries: ['*']
